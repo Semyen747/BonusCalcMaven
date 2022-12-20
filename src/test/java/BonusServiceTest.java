9276fa260417b42fde9a159db6395fb3df97c27e
Semyen747 <combatpilot45@gmail.com>
2022-12-20 20:59:12 +0300

    add tests for app

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -35,4 +35,49 @@ public class BonusServiceTest {
         // производим проверку (сравниваем ожидаемый и фактический):
         assertEquals(expected, actual);
     }
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForUnregisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+        // подготавливаем данные:
+        long amount = 1000_60;
+        boolean registered = false;
+        long expected = 10;
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+        // производим проверку (сравниваем ожидаемый и фактический результаты)
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForUnregisteredAndOverLimit() {
+        BonusService service = new BonusService();
+        // подготавливаем данные:
+        long amount = 1_000_000_60;
+        boolean registered = false;
+        long expected = 500;
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+        // производим проверку (сравниваем ожидаемый и фактический результаты)
+        assertEquals(expected, actual);
+    }
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForRegisteredAndBorderLimit() {
+        BonusService service = new BonusService ();
+        // подготавливаем данные:
+        long expected = 0;
+        // вызываем целевой метод:
+        long actual = service.calculate ( 0, true );
+        // производим проверку (сравниваем ожидаемый и фактический результаты)
+        assertEquals ( expected, actual );
+    }
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForUnregisteredAndBorderLimit() {
+        BonusService service = new BonusService();
+        // подготавливаем данные:
+        long expected = 0;
+        // вызываем целевой метод:
+        long actual = service.calculate(0, false);
+        // производим проверку (сравниваем ожидаемый и фактический результаты)
+        assertEquals(expected, actual);
+    }
 }
